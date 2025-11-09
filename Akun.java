3e217a89940fe293cf31e7752c23cb6a5d45b785
Fathur9208 <pahmi24@mhs.usk.ac.id>
2025-11-09 15:26:43 +0700

    Create Akun.java

---
+++ Akun.java
@@ -0,0 +1,25 @@
+public abstract class Akun {
+    private int id; 
+    private String nama;
+    private String password;
+
+    public Akun(int id, String nama, String password) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+    }
+    public abstract void tampilkanMenuAksi();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+  
+  
