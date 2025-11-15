3abdc0f5fd691e66080eda36832dd7d81986b1ee
Fathur <fathur9208@gmail.com>
2025-11-15 16:12:45 +0700

    tetr

--- run.bat
+++ run.bat
@@ -1,2 +1,27 @@
-javac -cp .;lib\gson-2.10.1.jar src\**\*.java src\*.java
-java -cp src;lib\gson-2.10.1.jar RestaurantDriver
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET CP_LIBS=.;lib\gson-2.10.1.jar
+SET MAIN_CLASS=RestaurantDriver
+
+
+SET SOURCES=
+FOR /R src %%f IN (*.java) DO (
+    SET SOURCES=!SOURCES! "%%f"
+)
+
+IF NOT DEFINED SOURCES (
+    goto :EXECUTION
+)
+
+javac -cp %CP_LIBS% %SOURCES% -d src
+
+IF ERRORLEVEL 1 (
+    goto :END
+)
+
+:EXECUTION
+
+java -cp src;lib\gson-2.10.1.jar %MAIN_CLASS%
+
+:END
+ENDLOCAL
