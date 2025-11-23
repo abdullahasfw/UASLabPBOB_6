e532482b2ef175619e0cd1a1338c9e273694cfa8
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 17:27:58 +0700

    l

--- src/akun/Pelayan.java
+++ src/akun/Pelayan.java
@@ -1,6 +1,7 @@
 package akun;
-public class pelayan extends Pegawai {
+public class Pelayan extends Pegawai {
     
-    public pelayan(int id, String nama, String password, String peran) {
+    public Pelayan(int id, String nama, String password, String peran) {
         super(id, nama, password, peran);
     }
+}
