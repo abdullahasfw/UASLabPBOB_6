09e43e084349cfef732e5f1ec09085b0cdedbc1c
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 17:24:44 +0700

    class kasir

--- src/akun/Kasir.java
+++ src/akun/Kasir.java
@@ -1,14 +1,9 @@
 package akun;
 public class Kasir extends Pegawai {
-    private String peran;
-    public Kasir(int id, String nama, String password, String peran) {
-        super(id, nama, password);
-        this.peran = "kasir";
-    }
-
-    public String getPeran() {
-        return peran;
-    }
-
     
+    public Kasir(int id, String nama, String password, String peran) {
+        super(id, nama, password, peran);
+    }
+
+
 }
