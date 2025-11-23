2f978012d35ac5cd356126587906860b1cb996ec
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 18:40:56 +0700

    cashpayment method

--- src/pembayaran/CashPayment.java
+++ src/pembayaran/CashPayment.java
@@ -1,10 +1,18 @@
 package pembayaran;
 public class CashPayment implements Pembayaran {
     private int id;
+
+    public CashPayment(int id) {
+        this.id = id;
+    }
     public void setID(int id) {
         this.id = id;
     }
     public int getID() {
         return id;
+    }
+
+    public void bayarCash(double jumlah) {
+        System.out.println("Cash Payment sebesar " + jumlah + " berhasil dilakukan dengan ID: " + id);
     }
 }
