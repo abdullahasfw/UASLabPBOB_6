e6677719665513106b68fe23fb9b4936284fec49
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 17:30:46 +0700

    koki

---
+++ src/akun/Koki.java
@@ -0,0 +1,9 @@
+package akun;
+public class Koki extends Pegawai{
+   
+    public Koki(int id, String nama, String password, String peran) {
+        super(id, nama, password, peran);
+    }
+    
+    
+}
