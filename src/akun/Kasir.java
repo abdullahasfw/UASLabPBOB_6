14724937d1dd034447557699ac2aa301cec08096
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 20:17:47 +0700

    boolean kasir, koki

--- src/akun/Kasir.java
+++ src/akun/Kasir.java
@@ -1,9 +1,21 @@
 package akun;
+import static akun.Pegawai.PERAN_VALID;
+
 public class Kasir extends Pegawai {
     
     public Kasir(int id, String nama, String password, String peran) {
         super(id, nama, password, peran);
+        if (!isPeranValid(peran)) {
+            throw new IllegalArgumentException("Peran'" + peran + "' tidak valid!");
+        }
     }
 
-
+     private boolean isPeranValid(String peran) {
+        for (String p : PERAN_VALID) {
+            if (p.equalsIgnoreCase(peran)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
