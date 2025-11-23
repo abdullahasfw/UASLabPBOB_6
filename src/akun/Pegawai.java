e02d131732ca76f5042378d42d4095abbd5e0283
syifatya <syifaathiyyaa@gmail.com>
2025-11-23 20:02:23 +0700

    boolean pegawai

--- src/akun/Pegawai.java
+++ src/akun/Pegawai.java
@@ -5,12 +5,31 @@ import sistem.RestaurantSystem;
 public class Pegawai extends Akun {
     private boolean isOnline;
     private String peran;
-    
+
+    // daftar peran valid
+    private static final String[] PERAN_VALID = {"kasir", "koki", "pelayan"};
 
     public Pegawai(int id, String nama, String password, String peran) {
         super(id, nama, password);
+
+        if (!isPeranValid(peran)) {
+            throw new IllegalArgumentException("Peran'" + peran + "' tidak valid!");
+        }
+
         this.peran = peran;
-        
+    }
+
+    private boolean isPeranValid(String peran) {
+        for (String p : PERAN_VALID) {
+            if (p.equalsIgnoreCase(peran)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getPeran() {
+        return peran;
     }
 
     public void updateStatusPesanan() {
